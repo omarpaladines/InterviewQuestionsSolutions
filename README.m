9a3b4582fb8da9a2acf231970de5dbfe68414a7b
Omar Paladines <omarpaladines@Omars-MacBook-Pro.local>
2016-07-01 21:58:50 -0700

    removed old readme

--- README.m
+++
@@ -1,3 +0,0 @@
-# InterviewQuestionsSolutions
-These are my solutions for the problems in Cracking the Coding Interview. The repository is organized by chapters and I have included test files for each class.
-In some cases I have also included a text file with comments for insight, other ideas and impressions about problems.
